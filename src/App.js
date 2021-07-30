4ba1ff1b62a22a0b7aa8f4f32b88cae712c56799
Apple <apple@redlimesolutions.com>
2021-07-30 21:07:37 +0600

    bootstrap added to react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <h1>Ecomm Project</h1>
+      <button className="btn btn-info mr-3">Test button</button>
+      <Button>React Bootstrap button </Button>
     </div>
   );
 }
